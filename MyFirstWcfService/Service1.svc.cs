e6c1a6ddb38aae684d9842ba5765435d50c5d3ee
wandriey <jona434g@edu.easj.dk>
2018-02-12 10:59:39 +0100

    getmyname metode implenteret.

--- MyFirstWcfService/Service1.svc.cs
+++ MyFirstWcfService/Service1.svc.cs
@@ -12,22 +12,13 @@ namespace MyFirstWcfService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        /// <summary>
+        /// Returnere mit Navn
+        /// </summary>
+        /// <returns></returns>
+        public string getMyName()
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return "Jonas";
         }
     }
 }
