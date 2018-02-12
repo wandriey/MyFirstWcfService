70cd5d4850c48211cdf5cd0b1cc96861be3f5ac4
wandriey <jona434g@edu.easj.dk>
2018-02-12 11:23:00 +0100

    GetEmailTest oprettet

--- MyFirstWcfServiceTests/Service1Tests.cs
+++ MyFirstWcfServiceTests/Service1Tests.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstWcfService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyFirstWcfServiceTests
+namespace MyFirstWcfService.Tests
 {
-    class Service1Tests
+    [TestClass()]
+    public class Service1Tests
     {
+        [TestMethod()]
+        public void getMyEmailTest()
+        {
+            //Arrange
+            var service1 = new Service1();
+
+            //act
+            string email = service1.getMyEmail();
+
+            //Assert
+            Assert.AreEqual("jonasmanley@hotmail.com", email);
+        }
+
+        [TestMethod()]
+        public void getMyNameTest()
+        {
+            Assert.Fail();
+        }
     }
 }
